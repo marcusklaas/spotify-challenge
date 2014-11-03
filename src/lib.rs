e05999c7d06396c185f92c8c34caba886121327b
Marcus Klaas <mail@marcusklaas.nl>
2014-11-03 23:36:35 +0100

    constructie van bipartite graaf

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,26 @@
 pub mod voter_input {
     use std::io;
 
+    #[deriving(Clone)]
     pub struct DogLover {
-        favorite_dog: uint,
-        hated_cat: uint
+        pub favorite_dog: uint,
+        pub hated_cat: uint
     }
 
+    #[deriving(Clone)]
     pub struct CatLover {
-        favorite_cat: uint,
-        hated_dog: uint
+        pub favorite_cat: uint,
+        pub hated_dog: uint
     }
 
+    #[deriving(Clone)]
     pub enum Voter {
         DogPerson(DogLover),
         CatPerson(CatLover)
     }
     
     impl Voter {
-        fn is_compatible(&self, other_voter: &Voter) -> bool {
+        pub fn is_compatible(&self, other_voter: &Voter) -> bool {
             match *self {
                 DogPerson(dog_lover_self) => match *other_voter {
                     DogPerson(..) => true,
@@ -83,19 +86,10 @@ pub mod voter_input {
     }
     
     // todo: only accept pets with number at most number of cats/ dogs
-    pub fn fill_voter_list(mut voter_list: Vec<Voter>, voter_count: uint) -> Vec<Voter> {
-        let voter_count_int: int = match voter_count.to_int() {
-            Some(x) => x,
-            None    => {
-                fail!("Could not cast unsigned integer to signed integer!");
-            }
-        };
-
-        for x in range(0i, voter_count_int) {
-            voter_list.push(get_voter());
+    pub fn fill_voter_list(voter_list: &mut [Voter], voter_count: uint) {
+        for x in range(0, voter_count) {
+            voter_list[x] = get_voter();
         }
-        
-        voter_list
     }
 
     fn get_voter() -> Voter {
@@ -160,4 +154,111 @@ pub mod voter_input {
     
         Some( Pet { species: species, number: number } )
     }
+}
+
+pub mod bipartite_matchings {
+    use std::collections::TreeSet;
+    use std::fmt;
+    
+    pub struct BipartiteGraph<'a> {
+        m: uint,
+        pub incidence_matrix: &'a mut [bool]
+    }
+    
+    impl<'a> BipartiteGraph<'a> {
+        pub fn new<'a>(m: uint, n: uint, vec: &'a mut Vec<bool>) -> BipartiteGraph<'a> {
+            vec.clear();
+            
+            for _ in range(0, m * n) {
+                vec.push(false);
+            }
+        
+            BipartiteGraph {
+                m: m,
+                incidence_matrix: vec.as_mut_slice()
+            }
+        }
+        
+        pub fn set_edge(&mut self, i: uint, j: uint, edginess: bool) {
+            self.incidence_matrix[self.m * i + j] = edginess;
+        }
+        
+        fn get_edge(&self, i: uint, j: uint) -> bool {
+            self.incidence_matrix[self.m * i + j]
+        }
+    }
+    
+//    impl<'a> fmt::Show for BipartiteGraph<'a> {
+//        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//            // The `f` value implements the `Writer` trait, which is what the
+//            // write! macro is expecting. Note that this formatting ignores the
+//            // various flags provided to format strings.
+//            
+//            for row in self.incidence_matrix.as_slice().chunks(self.m).collect() {
+//                writeln!(f, "{}", row);
+//            }
+//        }
+//    }
+    
+    trait SymmetricDifferenceSet {
+        fn new_symmetric_difference(&self, other: &Self) -> Self;
+    }
+    
+    #[deriving(Eq, Ord, Clone)]
+    pub struct Edge(uint, uint);
+    
+    impl PartialEq for Edge {
+        fn eq(&self, other: &Edge) -> bool {
+            let Edge(self_first, self_last) = *self;
+            let Edge(other_first, other_last) = *other;
+            
+            self_first == other_first && self_last == other_last 
+            || self_first == other_last && self_last == other_first
+        }
+    }
+    
+    impl PartialOrd for Edge {
+        fn lt(&self, other: &Edge) -> bool {
+            let Edge(self_first, self_last) = *self;
+            let Edge(other_first, other_last) = *other;
+            
+            self_first < other_first
+            || self_first == other_first && self_last < other_last
+        }
+        
+        fn partial_cmp(&self, other: &Edge) -> Option<Ordering> {
+            match self.lt(other) {
+                true  => Some(Less),
+                false => match self.eq(other) {
+                    true  => Some(Equal),
+                    false => Some(Greater)
+                }
+            }
+        }
+    }
+    
+    pub type EdgeSet = TreeSet<Edge>;
+    
+//    impl<T: Ord + Clone> SymmetricDifferenceSet for TreeSet<T> {
+//        fn new_symmetric_difference(&self, other: &TreeSet<T>) -> TreeSet<T> {
+//            let mut set = TreeSet::new();
+            
+//            for x in self.symmetric_difference(other) {
+//                set.insert(x.clone());
+//            }
+        
+//            set
+//        }
+//    }
+    
+//    pub fn get_augmenting_path(graph: BipartiteGraph, matching: EdgeSet) -> Option<EdgeSet> {
+//        None
+//    }
+    
+//    pub fn get_max_matching_size(graph: BipartiteGraph, matching: EdgeSet) -> int {
+//        match get_augmenting_path(graph, matching) {
+//            None       => matching.len(),
+//            Some(path) => get_max_matching_size(graph, matching.new_symmetric_difference(path))
+//        }
+//    }
 }
