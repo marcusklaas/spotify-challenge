54ba62d9743cfaca481810c3bcb786d3f32dbb4c
Marcus Klaas <mail@marcusklaas.nl>
2014-11-05 18:07:40 +0100

    voeg falende test toe

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,13 @@
+extern crate spotify;
+
+use spotify::bipartite_matchings::BipartiteGraph;
+
 #[test]
-fn some_test() {
-    assert!(1u == 1u);
+fn test_graph_creation() {
+    let mut rows = range(0, 10i);
+    let mut columns = range(0u, 10);
+
+    let graph = BipartiteGraph::from_closure(&mut rows, &mut columns, |x,y| true);
+    
+    assert_eq!(graph.get_max_matching_size(), 10);
 }
