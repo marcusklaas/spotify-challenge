b6ba64096f254ecad9289db198b18da2c749821f
Marcus Klaas <mail@marcusklaas.nl>
2014-12-07 17:42:34 +0100

    fix test on complete graph

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,10 +4,10 @@ use spotify::bipartite_matchings::BipartiteGraph;
 
 #[test]
 fn test_graph_creation() {
-    let mut rows = range(0, 10i);
-    let mut columns = range(0u, 10);
+    let rows = &[0u, ..10];
+    let columns = &[0u, ..10];
 
-    let graph = BipartiteGraph::from_closure(&mut rows, &mut columns, |x,y| true);
+    let graph = BipartiteGraph::from_closure(rows, columns, |x,y| true);
     
     assert_eq!(graph.get_max_matching_size(), 10);
 }
